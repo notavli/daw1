df3fe0c2683bab1ecfa0160bf5734eca9e63f573
Ahsan Shabbir <ahsh3509@proven.local>
2023-10-02 14:58:59 +0200

    salario

---
+++ horastrabajas/src/horastrabajas/Horastrabajas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package horastrabajas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ahsh3509
+ */
+public class Horastrabajas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int horas,tarifas;
+        System.out.println("cantas horas has trabajdo");
+        Scanner pantalla= new Scanner (System.in);
+        horas = pantalla.nextInt();
+     
+    }
+    
+}
