ec2e70b68e681ef6286a86b6033e1a2dc38c43cb
Ahsan Shabbir <ahsh3509@proven.local>
2023-10-03 12:51:57 +0200

    nueva

--- horastrabajas/src/horastrabajas/Horastrabajas.java
+++ horastrabajas/src/horastrabajas/Horastrabajas.java
@@ -16,31 +16,53 @@ public class Horastrabajas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    double horas,hora_normal,horas_extra,salario_extra,salario_normal,salario,t_normal;
+    double horas,hora_normal,horas_extra,salario_extra,salario_normal,salario,t_normal,impuesto,salario_impuesto,salario_sin_impuesto,salario_tope_impuesto;
     Scanner sc=new Scanner(System.in);
     System.out.print("igresa el numero de horas trabajados");
     horas=sc.nextInt();
      
     hora_normal=35;
     t_normal=20;
-     
-   if (horas>hora_normal)
+    salario_sin_impuesto=500;
+    salario_tope_impuesto=900;
+    if (horas>hora_normal)
     {
-         horas_extra = horas - hora_normal;
-         salario_extra = horas_extra*t_normal* 1.5;
-         salario_normal= hora_normal * t_normal;
-         salario = salario_extra + salario_normal;
+        horas_extra = horas - hora_normal;
+        salario_extra = horas_extra*t_normal* 1.5;
+        salario_normal= hora_normal * t_normal;
+        salario = salario_extra + salario_normal;
          
-         System.out.print("tu salario  es"+salario);
-     }
-     else
-     {
-         salario= horas*t_normal;
-         System.out.print("tu salario  es"+salario);
-     }
-      
-     
-     
+        System.out.print("tu salario  es "+salario);
+    }
+    else
+    {
+        salario= horas*t_normal;
+        System.out.print("tu salario  es "+salario);
+    }
+
+    if(salario>salario_tope_impuesto)
+        
+        salario_tope_impuesto=salario-salario_tope_impuesto;
+        impuesto=  salario_tope_impuesto*0.45;
+        salario_impuesto=  salario_tope_impuesto - salario_sin_impuesto;
+    
+    
+    
+    
+    
+    
+    if(salario>salario_sin_impuesto)
+    {
+        salario_impuesto = salario-salario_sin_impuesto;
+        impuesto=salario_impuesto*25/100;
+        System.out.print("tus impuesto  son "+impuesto);
+    }
+    else
+    {
+       impuesto =0;
+    }
+     salario= salario-impuesto;
+     System.out.print("salario bruto "+salario);
     }
     
 }
